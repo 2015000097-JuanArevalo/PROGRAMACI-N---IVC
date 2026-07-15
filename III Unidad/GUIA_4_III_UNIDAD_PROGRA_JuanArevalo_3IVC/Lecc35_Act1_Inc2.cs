69d4202e5feee1f977c195aafca400ff260d438e
2015000097 <j.arevalo.5ddb@gmail.com>
2026-07-15 09:04:41 -0600

    Add files via upload

---
+++ III Unidad/GUIA_4_III_UNIDAD_PROGRA_JuanArevalo_3IVC/Lecc35_Act1_Inc2.cs
@@ -0,0 +1,24 @@
+﻿internal class Program
+{
+    private static void Main(string[] args)
+    {
+
+        Console.WriteLine("Nombre: Juan Arévalo\nGrado: IVC - Clave: 3\n===CALCULADORA DE CAMBIO===");
+        int pago, precio;
+        string producto;
+        Console.Write("DIgite el nombre del producto: ");
+        producto = Console.ReadLine();
+        Console.WriteLine("Digite el precio del producto: ");
+        precio = Convert.ToInt32(Console.ReadLine());
+        Console.WriteLine("Digite el valor del billete con el que pagará: ");
+        pago =  Convert.ToInt32(Console.ReadLine());
+
+        Console.WriteLine("Menú Principal");
+        Console.WriteLine("1. Milímetros\n2. Centímetros\n3. Decímetros\n4. Hectómetros\n5. Kilómetros\n6. Salir");
+        Console.Write("Seleccione una opción: ");
+        int opc = Convert.ToInt32(Console.ReadLine());
+
+        int cambio = pago - precio;
+        Console.WriteLine("Su cambio es: " + cambio);
+    }
+}
